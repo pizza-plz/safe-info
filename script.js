5a5b278ed099e89893dd2c75796c8c0d802028d4
Hnry <grimset@tutanota.com>
2020-09-08 22:56:10 -0700

    hardware sensor scripts

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ const myLong = "-71.17609710203855";
 //
 //
 //
+// hardware sensors
+setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("temperature").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/temperature", true);
+    xhttp.send();
+  }, 10000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+
+
+
 //greeting
 function greeting() {
     var thetime = new Date();
